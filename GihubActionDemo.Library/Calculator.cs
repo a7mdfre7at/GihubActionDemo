87073ef07427581b553022ba7b0b9aa7239f2878
Ahmad Al-freihat <ahmad_freihat@outlook.com>
2021-12-16 02:43:23 +0200

    add test workflow

--- GihubActionDemo.Library/Calculator.cs
+++ GihubActionDemo.Library/Calculator.cs
@@ -18,7 +18,7 @@
         public int Multiply(int num1, int num2)
         {
             return num1 * num2;
-        }-
+        }
 
         // Subtracts small number from big number  
         public int Subtract(int num1, int num2)
